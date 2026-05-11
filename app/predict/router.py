f3ef25201b92c2fd08d8b7189908b490dfe19044
Juan Cruz <jcruzhe@uninpahu.edu.co>
2026-05-11 14:00:05 -0500

    refactor: update import paths and reorganize model loading in router.py

--- app/predict/router.py
+++ app/predict/router.py
@@ -1,25 +1,25 @@
 from pydantic import BaseModel
 from fastapi import APIRouter
 import pickle
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+model = pickle.load(open(BASE_DIR / "modelColab" / "modelo.pkl", "rb"))
+vectorizer = pickle.load(open(BASE_DIR / "modelColab" / "vectorizer.pkl", "rb"))
+
+
+routerModel = APIRouter(prefix="/model", tags=["Model"])
 
 
 class Message(BaseModel):
     message: str
 
 
-# Cargar el modelo y el vectorizador
-model = pickle.load(open("modelColab/modelo.pkl", "rb"))
-vectorizer = pickle.load(open("modelColab/vectorizer.pkl", "rb"))
-
-
-routerModel = APIRouter(prefix="/model", tags=["Model"])
-
-
 @routerModel.post("/message")
 def message(data: Message):
-    message = data.message
+    message_text = data.message
 
-    texto_vector = vectorizer.transform([message])
+    texto_vector = vectorizer.transform([message_text])
     prediccion = model.predict(texto_vector)[0]
 
-    return {"message": message, "prediction": prediccion}
+    return {"message": message_text, "prediction": prediccion}
